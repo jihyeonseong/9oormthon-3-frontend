e086383e79f3f1c24b9cb8ac2bd224fa4a62624a
ckhe1215 <ckhe1215@gmail.com>
2025-12-04 10:11:39 +0900

    chore: tailwind, vapor theme 세팅 및 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Button, VStack, Text } from "@vapor-ui/core";
+import "./index.css";
+import { SmallButton } from "./components/SmallButton";
+
 export function App() {
   return (
     <div className="App">
       <h1>User Management System</h1>
+      <VStack gap="$100">
+        <Text>테스트</Text>
+        <Button colorPalette="primary">버튼</Button>
+        <SmallButton></SmallButton>
+      </VStack>
     </div>
   );
 }
---
+++ src/components/SmallButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@vapor-ui/core";
+
+export const SmallButton = () => {
+  return (
+    <Button size="sm" variant="outline">
+      작은 버튼
+    </Button>
+  );
+};
